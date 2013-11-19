57c07908fdd4d872d1f042a4351aa0ae32bf832b
KimLandeweer <KRELandeweer@gmail.com>
2013-11-19 15:58:03 +0100

    Update

--- project.py
+++ project.py
@@ -133,72 +133,7 @@ while True:
         y+=movey
 
         pygame.display.update()
-=======
-	random_pos = (randint(0,WIDTH), randint(0,HEIGHT))
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
 
-			#check of een toets is ingedrukt, if so, check welke en plaats het Huisje
-			#alle maten van huizen zijn tijdelijk gedeeld door 5 zodat het allemaal mooi op het scherm past.
-			#randint zet ze random op het scherm - de breedte en hoogte van het huis+verplichtvrst zodat het allemaal op de map valt. 
-		if event.type == KEYDOWN:
-			# toetsen: E = eengezinsswoning, M = Maison, B = Bungalow
-			if event.key == K_e: #eensgezins
-				pygame.draw.rect(screen, BLUE, (randint(24,WIDTH-24),randint(24,HEIGHT-24), 24, 24))
-			elif event.key == K_b: #bungalow
-				pygame.draw.rect(screen, RED, (randint(32,WIDTH-32),randint(27,HEIGHT-27), 32, 27))
-			elif event.key == K_m: #maison
-				pygame.draw.rect(screen, GREEN, (randint(46,WIDTH-46),randint(45,HEIGHT-45), 46, 45))
-
-			#Om de huisjes ook handmatig te kunnen verplaatsen over het scherm, werkt nog niet, beginnetje
-			if event.key == K_LEFT:
-				movex = -1
-			elif event.key == K_RIGHT:
-				movex = +1
-			elif event.key == K_UP:
-				movey = -1
-			elif event.key == K_DOWN:
-				movey= +1
-		if event.type == KEYUP:
-			if event.key == K_LEFT:
-				movex = 0
-			elif event.key == K_RIGHT:
-				movex = 0
-			elif event.key == K_UP:
-				movey = 0
-			elif event.key == K_DOWN:
-				movey= 0
-
-		# Loop voor het plaatsen van huizen
-		# Komt straks in plaats van de handmatige plaatsing die er nu nog staat
-		# voor de x en y coordinaten in 'render(x, y)'  moet een functie komen die een random x en y produceert maar zorgt dat ze niet overlappen.
-	'''
-		#check huizenvariant en render de huizen
- 		Hvariant = TwintigH()
-		for i in range(1, Hvariant.eengezins):
-			name =  "e%d" % (i)
-			e = Eengezin()
-			e.name(name)
-			e.render(x, y)
-		for i in range(1, Hvariant.bungalow):
-			name = "b%d" % (i)
-			b = Bungalow()
-			b.name(name)
-			b.render(x, y)
-		for i in range(1, Hvariant.maison):
-			name= "m%d" % (i)
-			m = Maison()
-			m.name(name)
-			m.render(x, y)
-	'''
-
-	x+=movex
-	y+=movey
-
-	pygame.display.update()
->>>>>>> c871bb0f1dbd8bcbdf466d4f4981a91faece947a
 
 #------------------------------------------------------------------
 # Alvast idee voor de classes van de huizen (waarschijnlijk fout, moet nog even in OOP duiken van Python)
